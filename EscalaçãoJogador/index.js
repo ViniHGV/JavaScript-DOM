b049f3303a75358e51dbc7f37f7de9d22bb1588d
Vini <ixxvinicius@gmail.com>
2023-01-12 14:03:56 -0300

    obtendo o valor digitado dos inputs

--- EscalaçãoJogador/index.js
+++ EscalaçãoJogador/index.js
@@ -1,6 +1,10 @@
-function adicionar (){
-    const listaJogadores = document.getElementById('listaJogadores')
+function addPlayer (){
+    //Para obter o valor de um input usar "document.getElementById('id').value"
+    const position = document.getElementById('position').value
+    const name = document.getElementById('name').value
+    const number = document.getElementById('number').value
+    console.log(position+"\n"+ name+ "\n"+number)
+}
+function removePlayer(){
 
-    const h3 = document.createElement('h3')
-    h3.innerText = ("Jogador")
 }
