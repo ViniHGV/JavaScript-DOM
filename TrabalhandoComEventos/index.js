555161e57557202abc8c25f09edaacdcb00487b6
Vini <ixxvinicius@gmail.com>
2023-01-12 15:47:54 -0300

    Finalizando projeto

--- TrabalhandoComEventos/index.js
+++ TrabalhandoComEventos/index.js
@@ -1,5 +1,17 @@
+function register(ev){
+    console.log(ev)
+    const name = ev.currentTarget.parentNode.children.name.value
+    const password = ev.currentTarget.parentNode.children.password.value
+    const confirm = ev.currentTarget.parentNode.children.confirm.value
+
+    if (password === confirm){
+        alert("Usuário " + name + "Criado com sucesso !")
+        console.log({name, password,confirm})
+    }else {
+        alert("as senhas não conferem")
+    }
+}
+
 const button = document.getElementById('registerButton')
 
-button.addEventListener('click', function (){
-    alert("Botão clicado")
-})
+button.addEventListener('click', register)
