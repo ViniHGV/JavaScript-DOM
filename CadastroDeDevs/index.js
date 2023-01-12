1229f27b7ba22770c21417b4048e3b278a3881c1
Vini <ixxvinicius@gmail.com>
2023-01-12 17:11:35 -0300

    Criando o input de Tecnologia

--- CadastroDeDevs/index.js
+++ CadastroDeDevs/index.js
@@ -3,8 +3,18 @@ const form = document.getElementById('orderForm')
 form.addEventListener('submit', function(ev){
     ev.preventDefault()
     const name = document.querySelector('input[name="name"]').value
-    const email = document.querySelector('input[name="email"]').value
-    const password = document.querySelector('input[name="password"]').value
 
-    console.log({name,email,password})
+    const ul = document.createElement('ul')
+    const inputtec = document.createElement('input')
+    inputtec.type = "text"
+    inputtec.className = "form-control"
+    ul.appendChild(document.createElement('br'))
+    const title = document.createElement('p')
+    title.innerText = "Tecnologia: "
+    ul.appendChild(title)
+    ul.appendChild(inputtec)
+
+    
+
+    form.append(ul)
 })
