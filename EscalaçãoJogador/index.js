c7da1bf81b271db1614d7dbb9f80e11e9f12946c
Vini <ixxvinicius@gmail.com>
2023-01-12 14:32:38 -0300

    terminando a função addPlayer

--- EscalaçãoJogador/index.js
+++ EscalaçãoJogador/index.js
@@ -3,7 +3,26 @@ function addPlayer (){
     const position = document.getElementById('position').value
     const name = document.getElementById('name').value
     const number = document.getElementById('number').value
-    console.log(position+"\n"+ name+ "\n"+number)
+    console.log({position,name,number})
+
+    const confirmation = confirm("Escalar " + name + " como " + position + "?")
+
+    if (confirmation){
+        const teamList = document.getElementById('teamList')
+
+        //Criando um elemento
+        const playerItem = document.createElement('li')
+        
+        playerItem.id ='player-' + number
+        playerItem.innerText = position + ": " + name + " (" + number +")"
+
+        //inserir a team list com playerItem
+        teamList.appendChild(playerItem)
+
+        document.getElementById('position').value = ''
+        document.getElementById('name').value = ''
+        document.getElementById('number').value = ''
+    }
 }
 function removePlayer(){
 
