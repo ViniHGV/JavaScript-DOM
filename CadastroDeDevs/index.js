329b5e4f2c84fabf863f4d5d24e90037d3ddb940
Vini <ixxvinicius@gmail.com>
2023-01-12 18:54:38 -0300

    Adicionando o botão de salvar

--- CadastroDeDevs/index.js
+++ CadastroDeDevs/index.js
@@ -14,24 +14,31 @@ form.addEventListener('submit', function(ev){
     ul.appendChild(title)
     ul.appendChild(inputtec)
 
+
+
     const div = document.createElement('div')
     div.className = "form-check form-check-inline"
     const titleExp = document.createElement('div')
     const title2 = document.createElement('p')
     title2.innerText = "Tempo de Experiência"
+
     const inputcheck = document.createElement('input')
     inputcheck.className = "form-check-input"
     inputcheck.type = "radio"
     inputcheck.id = "inlineRadio1"
     inputcheck.name = "inlineRadioOptions"
+
     const lbl = document.createElement('label')
     lbl.className = "form-check-label"
     lbl.for = "inlineRadio1"
     lbl.innerText="0-2 Anos"
+
     div.appendChild(document.createElement('br'))
     titleExp.appendChild(title2)
     div.appendChild(inputcheck)
     div.appendChild(lbl)
+
+
 
     const div2 = document.createElement('div')
     div2.className = "form-check form-check-inline"
@@ -40,13 +47,17 @@ form.addEventListener('submit', function(ev){
     inputcheck2.type = "radio"
     inputcheck2.id = "inlineRadio2"
     inputcheck2.name = "inlineRadioOptions"
+
     const lbl2 = document.createElement('label')
     lbl2.className = "form-check-label"
     lbl2.for = "inlineRadio2"
     lbl2.innerText="3-4 Anos"
+
     div2.appendChild(document.createElement('br'))
     div2.appendChild(inputcheck2)
     div2.appendChild(lbl2)
+
+
 
     const div3 = document.createElement('div')
     const divbtn = document.createElement('div')
@@ -54,23 +65,47 @@ form.addEventListener('submit', function(ev){
     btn.type = "button"
     btn.className = "btn btn-danger"
     btn.innerText = "Excluir essa Linha"
+
+    const btnS = document.createElement('button')
+    btnS.type = "button"
+    btnS.className = "btn btn-primary"
+    btnS.innerText = "Excluir essa Linha"
+
     div3.className = "form-check form-check-inline"
     const inputcheck3 = document.createElement('input')
     inputcheck3.className = "form-check-input"
     inputcheck3.type = "radio"
     inputcheck3.id = "inlineRadio2"
     inputcheck3.name = "inlineRadioOptions"
+
     const lbl3 = document.createElement('label')
     lbl3.className = "form-check-label"
     lbl3.for = "inlineRadio2"
     lbl3.innerText="5+ Anos"
+
     div3.appendChild(document.createElement('br'))
     div3.appendChild(inputcheck3)
     div3.appendChild(lbl3)
+
     div3.appendChild(document.createElement('br'))
     divbtn.appendChild(document.createElement('br'))
     divbtn.appendChild(btn)
+    divbtn.appendChild(document.createElement('hr'))
 
 
     form.append(ul, titleExp, div ,div2, div3, divbtn)
+
+    form.addEventListener('button', function(){
+        const form = document.getElementById('orderForm')
+    
+        const ul = document.getElementsByTagName('ul')
+        const titleExp = document.getElementsByTagName('titleExp')
+        const div = document.getElementsByTagName('div')
+        const div2 = document.getElementsByTagName('div2')
+        const div3 = document.getElementsByTagName('div3')
+        const divbtn = document.getElementsByTagName('divbtn')
+        alert("Chegou no danger!")
+        form.remove()
+    })
 })
+
