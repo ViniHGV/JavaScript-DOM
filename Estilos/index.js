33b97d8c2b6ab03e331ef160a675df2236025f74
Vini <ixxvinicius@gmail.com>
2023-01-12 21:56:24 -0300

    Finalizando o Projeto de tema

--- Estilos/index.js
+++ Estilos/index.js
@@ -2,10 +2,10 @@ document.getElementById('claro').addEventListener('click', TemaClaro)
 document.getElementById('escuro').addEventListener('click', TemaEscuro)
 
 function TemaClaro(){
-    console.log(document.body.style)
     document.body.style.color = 'black'
     document.body.style.backgroundColor = '#fff'
 }
 function TemaEscuro(){
-
+    document.body.style.color = '#fff'
+    document.body.style.backgroundColor = 'rgb(44, 44, 44)'
 }
